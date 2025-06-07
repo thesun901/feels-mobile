790916cba45549b65391e736426bfdd1ab8d75c4
therockey <kacperus.tomczyk@gmail.com>
2025-06-07 12:10:32 +0200

    justify title

--- lib/screens/about_screen.dart
+++ lib/screens/about_screen.dart
@@ -8,7 +8,7 @@ class AboutScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('About')),
+      appBar: AppBar(title: const Text('About'), centerTitle: true),
       body: Center(
         child: SingleChildScrollView(
           child: Column(
