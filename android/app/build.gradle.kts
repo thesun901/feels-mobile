8de1d63682794f16cd866fc0514dd1081d60dadc
therockey <kacperus.tomczyk@gmail.com>
2025-06-17 19:04:35 +0200

    Fix build config for release version of android apk

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -34,6 +34,8 @@ android {
         release {
             // TODO: Add your own signing config for the release build.
             // Signing with the debug keys for now, so `flutter run --release` works.
+            isShrinkResources = false
+            isMinifyEnabled = false
             signingConfig = signingConfigs.getByName("debug")
         }
     }
